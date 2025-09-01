1c78bf21d59df76f2a719416e98cdba2633939ba
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-09-01 20:17:47 -0300

    Add authentication

---
+++ src/main/java/com/security/jwt/jobboard/controllers/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.security.jwt.jobboard.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.security.jwt.jobboard.dtos.Authentication;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid Authentication data){
+
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/security/jwt/jobboard/dtos/Authentication.java
@@ -0,0 +1,5 @@
+package com.security.jwt.jobboard.dtos;
+
+public record Authentication(String login, String password) {
+
+}
