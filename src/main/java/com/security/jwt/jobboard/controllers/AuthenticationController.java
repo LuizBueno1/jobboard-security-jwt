a01a2f81e840da463e3d74a8c78458c968400e07
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-09-03 19:51:11 -0300

    Add register method

--- src/main/java/com/security/jwt/jobboard/controllers/AuthenticationController.java
+++ src/main/java/com/security/jwt/jobboard/controllers/AuthenticationController.java
@@ -4,27 +4,54 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.security.jwt.jobboard.dtos.Authentication;
-
+import com.security.jwt.jobboard.dtos.AuthenticationRequest;
+import com.security.jwt.jobboard.dtos.RegisterRequest;
+import com.security.jwt.jobboard.models.user.UserModel;
+import com.security.jwt.jobboard.repositories.UserRepository;
+import com.security.jwt.jobboard.services.AuthorizationService;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
 
+    private final AuthorizationService authorizationService;
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid Authentication data){
+    @Autowired
+    private UserRepository userRepository;
 
-        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
+    AuthenticationController(AuthorizationService authorizationService) {
+        this.authorizationService = authorizationService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody @Valid AuthenticationRequest data){
+
+        UsernamePasswordAuthenticationToken usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        Authentication auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody @Valid RegisterRequest data){
+        
+        if(this.userRepository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        UserModel newUser = new UserModel(data.login(), encryptedPassword, data.role());
+
+        this.userRepository.save(newUser);
 
         return ResponseEntity.ok().build();
     }
